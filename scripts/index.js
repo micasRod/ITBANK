7090d0221c150e2b80e91c0dfa82bc4c8eb38da4
porlanagustin <porlan.agustin@gmail.com>
2022-06-17 20:29:24 -0300

    comienzo js

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+const dolar = [{
+    nombre: "Dolar oficial",
+    precioCompra: 0,
+    precioVenta: 0,
+    variacion: 0,
+},
+{
+    nombre: "Dolar Blue",
+    precioCompra: 0,
+    precioVenta: 0,
+    variacion: 0,
+},
+{
+    nombre: "Contado con Liqui",
+    precioCompra: 0,
+    precioVenta: 0,
+    variacion: 0,
+},
+{
+    nombre: "Dolar Promedio",
+    precioCompra: 0,
+    precioVenta: 0,
+    variacion: 0,
+},
+{
+    nombre: "Dolar Bolsa",
+    precioCompra: 0,
+    precioVenta: 0,
+    variacion: 0,
+},
+{
+    nombre: "Dolar Turista",
+    precioCompra: 0,
+    precioVenta: 0,
+    variacion: 0,
+},
+];
